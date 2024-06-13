6e47cde1a24038396fed79820d76ba1b48c99db9
sso <93847254+ssoq@users.noreply.github.com>
2024-06-13 08:03:28 +0100

    Custom particle sprite loading

--- Assets/Scripts/Management/CustomAssetLoader.cs
+++ Assets/Scripts/Management/CustomAssetLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 /// <summary>
@@ -9,21 +10,28 @@ using UnityEngine;
 /// a text document which contains the state of custom assets.
 /// </summary>
 
-public enum CustomPlayerOne
+public enum TypePlayerOne
 {
     Custom,
     Mod,
     Default //fallback
 }
 
-public enum CustomPlayerTwo
+public enum TypePlayerTwo
 {
     Custom,
     Mod,
     Default //fallback
 }
 
-public enum CustomNoteAsset
+public enum TypeNoteAsset
+{
+    Custom,
+    Mod,
+    Default //fallback
+}
+
+public enum TypeNoteParticleAsset
 {
     Custom,
     Mod,
@@ -32,5 +40,73 @@ public enum CustomNoteAsset
 
 public class CustomAssetLoader : MonoBehaviour
 {
-    
+    public TypePlayerOne _typePlayerOne;
+    public TypePlayerTwo _typePlayerTwo;
+    public TypeNoteAsset _typeNoteAsset;
+    public TypeNoteAsset _typeNoteParticleAsset;
+
+    #region Note Renderer Section
+
+    public string CustomNoteParticleName;
+    [Header("Note Particle Renderers")]
+    [SerializeField] private List<SpriteRenderer> _noteParticleSpriteRenderer;
+
+    #endregion
+
+    private void Awake()
+    {
+        GatherNeededObjects();
+    }
+
+    private void GatherNeededObjects()
+    {
+        GameObject[] _noteParticles = GameObject.FindGameObjectsWithTag("Note Particle");
+        for (int i = 0; i < _noteParticles.Length; i++)
+        {
+            SpriteRenderer spriteRenderer = _noteParticles[i].GetComponent<SpriteRenderer>();
+            _noteParticleSpriteRenderer.Add(spriteRenderer);
+        }
+
+        LoadCustomAssets(); // calls after for-loops
+    }
+
+    private void LoadCustomAssets()
+    {
+        switch (_typeNoteAsset)
+        {
+            case TypeNoteAsset.Custom:
+                for (int i = 0; i < _noteParticleSpriteRenderer.Count; i++)
+                {
+                    _noteParticleSpriteRenderer[i].sprite = LoadStreamedSprite($"" +
+                        $"{Application.streamingAssetsPath + "/Sprites/UI/Game/NoteParticles/"}", CustomNoteParticleName + ".png");
+                }
+                break;
+            case TypeNoteAsset.Mod:
+                break;
+            case TypeNoteAsset.Default:
+                break;
+        }
+    }
+
+    public Sprite LoadStreamedSprite(string filepath, string fileName)
+    {
+        string filePath = Path.Combine(filepath, fileName);
+
+        if (File.Exists(filePath))
+        {
+            byte[] fileData = File.ReadAllBytes(filePath);
+
+            Texture2D texture = new (1080, 1080, TextureFormat.ARGB32, false);
+            texture.LoadImage(fileData);
+
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+
+            return sprite;
+        }
+        else
+        {
+            Debug.LogError("Could not find any file with the given path " + filePath);
+            return null;
+        }
+    }
 }
