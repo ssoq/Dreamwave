813d0d0058d81d14d45d087f836e840bf60207ba
sso <93847254+ssoq@users.noreply.github.com>
2024-06-14 09:07:14 +0100

    Gave enemy note warnings

--- Assets/Scripts/Mechanics/IncomingNoteWarning.cs
+++ Assets/Scripts/Mechanics/IncomingNoteWarning.cs
@@ -37,7 +37,7 @@ public class IncomingNoteWarning : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Note"))
+        if (collision.gameObject.CompareTag("Note") | collision.gameObject.CompareTag("EnemyNote"))
         {
             _notesInSection.Add(collision.gameObject);
         }
@@ -45,7 +45,7 @@ public class IncomingNoteWarning : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Note"))
+        if (collision.gameObject.CompareTag("Note") | collision.gameObject.CompareTag("EnemyNote"))
         {
             _notesInSection.Remove(collision.gameObject);
         }
