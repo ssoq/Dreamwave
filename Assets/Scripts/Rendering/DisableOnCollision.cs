d2c2f397a0a418f04252d07598deb0a0cc8f978f
sso <93847254+ssoq@users.noreply.github.com>
2024-06-14 13:06:23 +0100

    Added side checks for hold notes and they no longer auto hold

--- Assets/Scripts/Rendering/DisableOnCollision.cs
+++ Assets/Scripts/Rendering/DisableOnCollision.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class DisableOnCollision : MonoBehaviour
 {
+    [SerializeField] private WhichSide _whichSide;
     [SerializeField] private string _colName;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag(_colName))
         {
-            gameObject.SetActive(false);
+            if (_whichSide == WhichSide.Left && Input.GetKey(GameManager.Instance.left))
+            {
+                gameObject.SetActive(false);
+            }
+
+            if (_whichSide == WhichSide.Down && Input.GetKey(GameManager.Instance.down))
+            {
+                gameObject.SetActive(false);
+            }
+
+            if (_whichSide == WhichSide.Up && Input.GetKey(GameManager.Instance.up))
+            {
+                gameObject.SetActive(false);
+            }
+
+            if (_whichSide == WhichSide.Right && Input.GetKey(GameManager.Instance.right))
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 }
